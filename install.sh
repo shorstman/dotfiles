bd8d6ec652a0f6162ebb11c629aafd8d837bacd4
shorstman <horstman.s@husky.neu.edu>
2017-10-09 12:25:57 -0400

    Updated config file with i3 gaps

--- install.sh
+++ install.sh
@@ -9,6 +9,22 @@ if [[ $1 == *"i"* ]]; then
   sudo apt install git
   sudo apt install python-pip
   sudo apt install rofi
+  sudo apt install xautolock
+  sudo apt install autoconf
+
+  #i3 gaps
+  sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake
+  cd ..
+  git clone https://www.github.com/Airblader/i3 i3-gaps
+  cd i3-gaps
+  autoreconf --force --install
+  rm -rf build/
+  mkdir -p build && cd build/
+  ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+  make
+  sudo make install
+  cd ..
+  cd dotfiles
 fi
 
 #Run config wizard to generate a config file
@@ -20,4 +36,6 @@ if [[ $1 == *"c"* ]]; then
   cp ./i3/i3blocks.conf ~/.config/i3/
   cp ./settings.ini ~/.config/gtk-3.0/
   cp ./.gtkrc-2.0 ~/
+  mkdir ~/Pictures/Wallpapers
+  cp ./wallpaper.png ~/Pictures/Wallpapers/
 fi
